3628d266e9c2bd99f8b2e04af6403c2597fec36e
Minaga Ekanayake <minaeka221@gmail.com>
2023-10-06 17:37:02 +0400

    Final Code

---
+++ Todo/Views/How Many Todos  Tab/HowManyMoreView.swift
@@ -0,0 +1,20 @@
+//
+//  HowManyMoreView.swift
+//  Todo
+//
+//  Created by Minaga Ekanayake on 16/08/2023.
+//
+
+import SwiftUI
+
+struct HowManyMoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HowManyMoreView_Previews: PreviewProvider {
+    static var previews: some View {
+        HowManyMoreView()
+    }
+}
---
+++ Todo/Views/Main Todo List Tab/MainTodoListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainTodoListView.swift
+//  Todo
+//
+//  Created by Minaga Ekanayake on 16/08/2023.
+//
+
+import SwiftUI
+
+struct MainTodoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainTodoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTodoListView()
+    }
+}
